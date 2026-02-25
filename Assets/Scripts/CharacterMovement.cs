f3593e7a98a655b15e2ceaf04a0cc6c5c60e80bd
ayakharchafi <143551995+ayakharchafi@users.noreply.github.com>
2026-02-25 13:29:24 -0500

    added the move left and right to the player
    next is to complete the animator to acc walk the player

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private float moveInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         moveInput = Input.GetAxisRaw("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0f);
     }
 }
